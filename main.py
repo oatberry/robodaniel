e5a7b1c63a42ade0cb51d538523b50e7dfc70690
Thomas Berryhill <oats@oatberry.me>
2017-06-09 21:32:33 -0400

    load api key from env var instead of file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import os
 from groupy import Bot, config
 
-with open('API_KEY.txt', 'r') as f:
-    config.API_KEY = f.readline()
+config.API_KEY = os.getenv('API_KEY')
 
 bot = Bot.list().first
 bot.post("bleep bloop, under construction. also, no Thomas here. just bot.")
