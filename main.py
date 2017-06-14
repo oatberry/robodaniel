e02b96d333213f6ce9000724a6abcf759a48ca00
Thomas Berryhill <oats@oatberry.me>
2017-06-14 19:32:47 -0400

    add rudimentary debugging/logging

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def listen(port=''):
     except:
         port = 5000
 
+    print('--> opening listener socket...')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -26,14 +27,17 @@ def listen(port=''):
 
         try:
             data = connection.recv(1024)
+            print('DATA: {0}'.format(data))
             data = json.loads(data.decode('utf-8').split('\n')[-1])
 
             if data['sender_type'] == "user" and data['text'][0] == '!':
+                print('--> received command: {0}'.format(data['text'][1:]))
                 interpret(data['text'][1:])
         except:
             pass
 
 
 if __name__ == '__main__':
+    print('--> launching robodaniel...')
     bot = Bot.list().first
     listen()
