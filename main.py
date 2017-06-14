45897d8607a353e5ee26b7bfe09e16900910af30
Thomas Berryhill <oats@oatberry.me>
2017-06-14 19:57:45 -0400

    moar debugging

--- main.py
+++ main.py
@@ -26,14 +26,15 @@ def listen(port=''):
         (connection, address) = s.accept()
 
         data = connection.recv(1024)
+        print('--> ROBODANIEL: raw data received: {0}'.format(data))
         data = json.loads(data.decode('utf-8').split('\n')[-1])
-        print('--> JSON data received: {0}'.format(data))
+        print('--> ROBODANIEL: json data received: {0}'.format(data))
 
         if data['sender_type'] == "user" and data['text'][0] == '!':
-            print('--> interpreted command: {0}'.format(data['text'][1:]))
+            print('--> ROBODANIEL: interpreted command: {0}'.format(data['text'][1:]))
             interpret(data['text'][1:])
 
 if __name__ == '__main__':
-    print('--> launching robodaniel...')
+    print('--> ROBODANIEL launching robodaniel...')
     bot = Bot.list().first
     listen()
