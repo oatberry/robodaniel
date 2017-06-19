25343f9dbbfbdd83bc58565875e7b039912d6ef7
Thomas Berryhill <oats@oatberry.me>
2017-06-19 12:03:32 -0400

    make !insult and !compliment a tiny bit more efficient

--- commands.py
+++ commands.py
@@ -1,43 +1,52 @@
 #
-# commands (not just factoids!) for robodaniel
+# commands for robodaniel
+# everything is returned in a tuple or a list, period!
 #
 
 def compliment(args, sender, sender_id, attachments, bot):
     '[person]: send someone a compliment!'
     from data.compliments import compliments
+    from helpers import give
     import random
+
+    compliment = random.choice(compliments)
 
     try:
         user_id = attachments[0]['user_ids'][0]
     except:
-        return [random.choice(compliments)]
+        return [compliment] # send a compliment anyway if no name is given
 
-    compliment = random.choice(compliments)
-    
-    return give(attachments[0]['user_ids'][0], compliment, bot)
+    # use give() helper function to construct a mention
+    return give(user_id, compliment, bot)
 
 def help(args, sender, sender_id, attachments, bot):
     '[command]: show available factoids and commands or help for a specific command'
     from data.factoids import factoids
     import commands, re
 
+    # get lists of all available factoids and commands
     factoid_list = list(factoids)
     command_list = [i for i in dir(commands) if not re.match('^__', i)]
 
     if len(args) == 0:
         return ['list of factoids: {}\nlist of commands: {}'.format(factoid_list, command_list)]
     else:
-        return [args[0] + ' ' + eval(args[0] + '.__doc__')]
+        return ['> {} {}'.format(args[0], eval(args[0] + '.__doc__'))]
 
 def insult(args, sender, sender_id, attachments, bot):
     '[person]: sendeth some lout a shakespearean fig!'
     import data.insults as insults, random
     from helpers import give
     
-    insult = 'Thou {} {} {}!'.format(random.choice(insults.part_1),
+    insult = 'thou {} {} {}!'.format(random.choice(insults.part_1),
                                      random.choice(insults.part_2),
                                      random.choice(insults.part_3))
-    return give(attachments[0]['user_ids'][0], insult, bot)
+    try:
+        user_id = attachments[0]['user_ids'][0]
+    except:
+        return [insult] # send an insult anyway if no name is given
+
+    return give(user_id, insult, bot)
 
 def rev(args, sender, sender_id, attachments, bot):
     '<string>: reverse a string of text'
