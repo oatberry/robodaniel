e952ace707e57b66aea0de79b77ab169aa11aa19
Thomas Berryhill <oats@oatberry.me>
2017-06-20 22:11:42 -0400

    follow pep8 standard for blank lines

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@
 # everything is returned in a tuple or a list, period!
 #
 
+
 def compliment(args, sender, sender_id, attachments, bot):
     '[person]: send someone a compliment!'
     from data.compliments import compliments
@@ -19,6 +20,7 @@ def compliment(args, sender, sender_id, attachments, bot):
     # use give() helper function to construct a mention
     return give(user_id, compliment, bot)
 
+
 def help(args, sender, sender_id, attachments, bot):
     '[command]: show available factoids and commands or help for a specific command'
     from data.factoids import factoids
@@ -32,6 +34,7 @@ def help(args, sender, sender_id, attachments, bot):
         return ['list of factoids: {}\nlist of commands: {}'.format(factoid_list, command_list)]
     else:
         return ['> !{} {}'.format(args[0], eval(args[0] + '.__doc__'))]
+
 
 def insult(args, sender, sender_id, attachments, bot):
     '[person]: sendeth some lout a shakespearean fig!'
@@ -48,6 +51,7 @@ def insult(args, sender, sender_id, attachments, bot):
 
     return give(user_id, insult, bot)
 
+
 def meme(args, sender, sender_id, attachments, bot):
     ': get a random viral meme from the last week off of imgur'
     import os, random, requests
@@ -62,6 +66,7 @@ def meme(args, sender, sender_id, attachments, bot):
 
     image_url = random.choice(memes)['link']
     return [image_url]
+
 
 def rev(args, sender, sender_id, attachments, bot):
     '<string>: reverse a string of text'
